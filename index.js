35a526f093fe4315e39f9ce07ef2edf33d6fed6c
AtanasChobanov <157892242+AtanasChobanov@users.noreply.github.com>
2024-10-01 21:56:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 var blogs = [];
 
 app.use(express.static("public"));
