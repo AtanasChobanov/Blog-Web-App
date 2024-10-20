1b9fe0455d86d520ce267bb3191859a50494d723
AtanasChobanov <157892242+AtanasChobanov@users.noreply.github.com>
2024-10-20 20:12:41 +0300

    Update port in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import methodOverride from "method-override";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 var channels = [{
   name: "Biology",
   admin: "Atanas",
