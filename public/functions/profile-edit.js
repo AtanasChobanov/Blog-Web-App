16b4153cb830150771f297fc2a9dc96e092739bf
Ivan122334 <124714486+Ivan122334@users.noreply.github.com>
2024-11-28 20:42:06 +0200

    Delete public/functions/profile-edit.js

--- public/functions/profile-edit.js
+++
@@ -1,7 +0,0 @@
-function openPopup() {
-    document.getElementById('passwordPopup').style.display = 'flex';
-}
-
-function closePopup() {
-    document.getElementById('passwordPopup').style.display = 'none';
-}
