40a544b8b8469d3bf82b6365e7c4727bbffcd13d
AtanasChobanov <atanaschobanov2008@gmail.com>
2024-12-30 15:03:54 +0200

    Update Google sign-in

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google",
+      callbackURL: "/auth/google",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       scope: ["profile", "email"],
     },
